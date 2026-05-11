0da7d170f55247457d314c6f737ddddd19ca702f
matsuoka natsuki <natsumugisunny@yahoo.co.jp>
2026-05-12 06:34:39 +0900

    Remove Unnecessary

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ContactController;
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\TagController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('categories', CategoryController::class);
-    Route::apiResource('tags', TagController::class);
     Route::apiResource('contacts', ContactController::class);
 });
