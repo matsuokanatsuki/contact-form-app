bb5ec71b6f08d6d834417a2b1a6574b4013d869a
matsuoka natsuki <natsumugisunny@yahoo.co.jp>
2026-05-12 09:41:06 +0900

    Revise Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,37 +3,28 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-
-        // ModelNotFoundExceptionをカスタマイズ
-        $this->renderable(function (ModelNotFoundException $e, $request) {
+        $this->renderable(function (
+            NotFoundHttpException $e,
+            $request
+        ) {
             if ($request->is('api/*')) {
                 return response()->json([
                     'message' => 'お問い合わせが見つかりませんでした。',
-                ], 404);
+                ], Response::HTTP_NOT_FOUND);
             }
         });
     }
